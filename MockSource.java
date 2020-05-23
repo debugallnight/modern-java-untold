f2bbd1411445aa2fa72c968c92f4aeb93ece9c48
Song Choe <debugallnight@gmail.com>
2020-05-23 17:03:00 -0400

    Create MockSource.java

---
+++ MockSource.java
@@ -0,0 +1,19 @@
+public class MockSource {
+	
+	@Getter
+	@Setter
+	private int count;
+	
+	@Getter
+	@Setter
+	private String id;
+	
+	@Getter
+	@Setter
+	private String name;
+	
+	public MockSource() {
+		
+	}
+
+}
